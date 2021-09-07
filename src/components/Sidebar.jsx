357c8defa79cafd223a2be09df47417923abbaa8
Vakha <gvardee95@gmail.com>
2021-09-07 20:54:54 +0300

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 function Sidebar({ products }) {
-    return (
-        <div className="sidebar">
-            <ul>
-                {products &&
-                    products.map((item, index) =>
-                        <li key={item + '_' + index}>
-                            <Link to={`/${item.rid}`}>{item.rname}</Link>
-                        </li>
-                    )
-                }
-            </ul>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <ul>
+        {products &&
+          products.map((item, index) => (
+            <li key={item + "_" + index}>
+              <Link to={`/products-table/${item.rid}`}>{item.rname}</Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
