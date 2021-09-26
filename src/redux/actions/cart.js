674567c079f0ddf8eec300fc7da16838c020ed27
Vakha <gvardee95@gmail.com>
2021-09-26 15:57:11 +0300

    Отрефакторил

--- src/redux/actions/cart.js
+++
@@ -1,6 +0,0 @@
-export const addToCart = (data) => {
-    return {
-        type: "ADD_TO_CART",
-        payload: data
-    }
-}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
-import { createStore, compose, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import rootReducer from './reducers'
+import rootReducer from "./features";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
